0467585b9b847023e15a41acc56a3beb256818e6
ItsBloo <jamesheald@protonmail.com>
2020-08-11 23:51:51 +0100

    Trying to get QR code reading to work from the webcam

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Webcam from 'react-webcam';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import QrScanner from 'qr-scanner';
+import WorkerPath from '!!file-loader!../../node_modules/qr-scanner/qr-scanner-worker.min.js';
+QrScanner.WORKER_PATH = WorkerPath;
+console.log(QrScanner.WORKER_PATH);
+class App extends Component{
+  constructor(props){
+    super();
+    this.videoReference = null;
+    this.setVideoElementRef = element => {
+      this.videoReference = element;
+    }
+  }
+
+  componentDidMount(){
+    const video = this.videoReference.video;
+    console.log(this.videoReference);
+    const scanner = new QrScanner(video, function(result){
+      console.log(result);
+    })
+
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to The Establishments Arms</h1>
+          <p>Help us aid the track and trace effort by showing this kiosc the barcode provided by the Traci app</p>
+          <Webcam width={320} height={240} style={{borderRadius:4}} ref={this.setVideoElementRef}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
