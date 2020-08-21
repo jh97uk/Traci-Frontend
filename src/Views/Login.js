742999213ad3438ff0d73eb3906068e6c8c5a616
James Heald <jh97uk@icloud.com>
2020-08-20 18:20:00 -0700

    Added sb-admin-2 bootstrap theme in preperation for admin control panel and made a start on template for login based off of sb-admin

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import '../sb-admin-2.css';
 
 class Login extends Component{
     constructor(props){
-
+        super();
     }
-
+    
     render(){
         return(
-            <div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-xl-10 col-lg-12 col-md-9">
+                        <div className="card border-0 shadow-lg my-5">
+                            <div className="card-body p-0">
+                                <div className="row">
+                                    <div className="col-lg-6">
+                                        <div className="p-5">
+                                            <div className="text-center">
+                                                <h1 className="h4 text-gray-900 mb-4">Login</h1>
+                                            </div>
+                                            <form className="user">
+                                                <div className="form-group">
+                                                    <input type="email" className="form-control form-control-user" id="exampleInputEmail" placeholder="Enter Email Address..."/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password"/>
+                                                </div>
+                                            </form>
+                                            <button className="btn btn-primary btn-user btn-block">Login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
