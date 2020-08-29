13832c0357ac187dda182b9d3f45af3076cf2142
James Heald <jh97uk@icloud.com>
2020-08-29 10:33:02 -0700

    ++ customer list with working entry & depart times

--- src/Views/DashboardLanding.js
+++ src/Views/DashboardLanding.js
@@ -3,10 +3,32 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
 
 class DashboardLanding extends Component{
     constructor(props){
         super(props);
+        this.state = {customers:[]}
+    }
+
+    componentDidMount(){
+        const self = this;
+        fetch('http://localhost:4000/customer/', {
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer '+localStorage.getItem('token')
+            }
+            }).then(function(response){
+                return response.json();
+            }).then(function(data){
+                self.setState({customers:data});
+                console.log(new Date(Date.parse(data[0].entryTimestamp)).getHours())
+            })
     }
 
     render(){
@@ -22,6 +44,16 @@ class DashboardLanding extends Component{
                             label="Search number"
                             defaultValue=""
                             style={{width:'100%'}}/>
+                            <List>
+                                {this.state.customers.map((item, index)=>(
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={item.phoneNumber}
+                                            secondary={new Date(Date.parse(item.entryTimestamp)).getHours() + ":" + new Date(Date.parse(item.entryTimestamp)).getMinutes() + " - " + (item.departureTimestamp ? (new Date(Date.parse(item.departureTimestamp)).getHours() + ":" + new Date(Date.parse(item.departureTimestamp)).getMinutes()) : 'N/A')}/>
+                                    </ListItem>
+                                ))}
+                                
+                            </List>
                         </CardContent>
                     </Card>
                 </Grid>
