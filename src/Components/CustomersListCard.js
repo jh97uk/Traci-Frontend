52f1e2a6df7c0a977d7715a4fd2d77f3955e80c1
James Heald <jh97uk@icloud.com>
2020-08-29 10:58:21 -0700

    Put the customer list into component

---
+++ src/Components/CustomersListCard.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+
+import TextField from '@material-ui/core/TextField';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+class CustomersListCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {customers:[]}
+    }
+
+    componentDidMount(){
+        const self = this;
+        fetch('http://localhost:4000/customer/', {
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer '+localStorage.getItem('token')
+            }
+            }).then(function(response){
+                return response.json();
+            }).then(function(data){
+                self.setState({customers:data});
+                console.log(new Date(Date.parse(data[0].entryTimestamp)).getHours())
+            })
+    }
+
+    render(){
+        return(
+            <Card>
+                <CardContent>
+                    <TextField
+                    id="outlined-required"
+                    label="Search number"
+                    defaultValue=""
+                    style={{width:'100%'}}/>
+                    <List>
+                        {this.state.customers.map((item, index)=>(
+                            <ListItem>
+                                <ListItemText
+                                    primary={item.phoneNumber}
+                                    secondary={new Date(Date.parse(item.entryTimestamp)).getHours() + ":" + new Date(Date.parse(item.entryTimestamp)).getMinutes() + " - " + (item.departureTimestamp ? (new Date(Date.parse(item.departureTimestamp)).getHours() + ":" + new Date(Date.parse(item.departureTimestamp)).getMinutes()) : 'N/A')}/>
+                            </ListItem>
+                        ))}  
+                    </List>
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+export default CustomersListCard;
--- src/Views/DashboardLanding.js
+++ src/Views/DashboardLanding.js
@@ -1,61 +1,27 @@
 import React, {Component} from 'react';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
+import Grid from '@material-ui/core/Grid';
+
+import CustomersListCard from '../Components/CustomersListCard.js';
+import { Card } from '@material-ui/core';
 
 
 class DashboardLanding extends Component{
     constructor(props){
         super(props);
-        this.state = {customers:[]}
     }
 
     componentDidMount(){
         const self = this;
-        fetch('http://localhost:4000/customer/', {
-            method:'GET',
-            headers:{
-                'Content-Type':'application/json',
-                'Authorization': 'Bearer '+localStorage.getItem('token')
-            }
-            }).then(function(response){
-                return response.json();
-            }).then(function(data){
-                self.setState({customers:data});
-                console.log(new Date(Date.parse(data[0].entryTimestamp)).getHours())
-            })
     }
 
     render(){
         return(
-                <Grid container style={{display:'flex', flex: "1 1 auto", flexDirection:"row", height:'100%', padding:20}}>
-                    <Card
+                <Grid container style={{height:'100%', padding:20}}>
+                    <CustomersListCard
                         component={Grid}
                         item
-                        xs={6}>
-                        <CardContent>
-                            <TextField
-                            id="outlined-required"
-                            label="Search number"
-                            defaultValue=""
-                            style={{width:'100%'}}/>
-                            <List>
-                                {this.state.customers.map((item, index)=>(
-                                    <ListItem>
-                                        <ListItemText
-                                            primary={item.phoneNumber}
-                                            secondary={new Date(Date.parse(item.entryTimestamp)).getHours() + ":" + new Date(Date.parse(item.entryTimestamp)).getMinutes() + " - " + (item.departureTimestamp ? (new Date(Date.parse(item.departureTimestamp)).getHours() + ":" + new Date(Date.parse(item.departureTimestamp)).getMinutes()) : 'N/A')}/>
-                                    </ListItem>
-                                ))}
-                                
-                            </List>
-                        </CardContent>
-                    </Card>
+                        xs={6}/>
                 </Grid>
         )
     }
