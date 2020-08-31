67ae64ca7fc62223838b89ca0b95a314586d0f58
James Heald <jh97uk@icloud.com>
2020-08-30 18:17:17 -0700

    Added active customers stat card

--- src/Views/DashboardLanding.js
+++ src/Views/DashboardLanding.js
@@ -11,7 +11,7 @@ import CustomersListCard from '../Components/CustomersListCard.js';
 class DashboardLanding extends Component{
     constructor(props){
         super(props);
-        this.state = {customerCount:null}
+        this.state = {customerCount:null, activeCustomerCount:null}
         this.updateSelectedDayCustomerNumber = this.updateSelectedDayCustomerNumber.bind(this);
     }
 
@@ -19,44 +19,78 @@ class DashboardLanding extends Component{
         const self = this;
     }
 
-    updateSelectedDayCustomerNumber(number){
-        this.setState({customerCount:number});
+    updateSelectedDayCustomerNumber(customerCount, activeCustomerCount){
+        this.setState({customerCount:customerCount, activeCustomerCount:activeCustomerCount});
     }
 
     render(){
         return(
-            <div style={{maxHeight:'100vh', padding:20, maxWidth:'100vh'}}>
+            <div style={{maxHeight:'100vh', padding:20}}>
                 <Grid container spacing={2}>
                     <Grid item xs={6}>
                         <CustomersListCard
                             customerCountSetter={this.updateSelectedDayCustomerNumber}/>
                     </Grid>
-                    <Grid item xs={6} direction="row">
-                        <Card
-                            component={Grid}
-                            item
-                            xs={12} 
-                            sm={12} 
-                            md={7}
-                            style={{backgroundColor:this.props.theme.palette.secondary.main, position:'relative'}}>
-                                <CardContent>
-                                    <Typography variant='h8' style={{color:'white', width:"100vh"}}>TODAYS CUSTOMERS</Typography>
-                                    <Typography variant='h3' style={{color:'white', width:"100vh"}}>{(this.state.customerCount !== null) ? this.state.customerCount : 'N/A'}</Typography>
-                                </CardContent>
-                                {(this.state.customerCount === null) && (<div 
-                                    style={{
-                                        position:'absolute',
-                                        top:0,
-                                        zIndex:123,
-                                        width:'100%',
-                                        height:'100%',
-                                        backgroundColor:'rgba(230, 230, 230, 0.9)',
-                                        display:'flex',
-                                        justifyContent:'center',
-                                        alignItems:'center'}}>
-                                    <CircularProgress></CircularProgress>
-                                </div>)}
-                            </Card>
+                    <Grid item xs={6}>
+                        <Grid container
+                            direction="row"
+                            xs={12}
+                            spacing={2}>
+                                <Grid
+                                    item
+                                    xs={12}
+                                    md={6}
+                                    lg={3}>
+                                    <Card
+                                        style={{backgroundColor:this.props.theme.palette.secondary.main, position:'relative', width:'100%', height:'100%'}}>
+                                            <CardContent>
+                                                <Typography variant='h8' style={{color:'white', width:"100vh"}}>TODAYS CUSTOMERS</Typography>
+                                                <Typography variant='h3' style={{color:'white', width:"100vh"}}>{(this.state.customerCount !== null) ? this.state.customerCount : 'N/A'}</Typography>
+                                            </CardContent>
+                                            {(this.state.customerCount === null) && (
+                                                <div 
+                                                    style={{
+                                                        position:'absolute',
+                                                        top:0,
+                                                        zIndex:123,
+                                                        width:'100%',
+                                                        height:'100%',
+                                                        backgroundColor:'rgba(230, 230, 230, 0.9)',
+                                                        display:'flex',
+                                                        justifyContent:'center',
+                                                        alignItems:'center'}}>
+                                                    <CircularProgress></CircularProgress>
+                                                </div>)}
+                                    </Card>
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={12}
+                                    md={6}
+                                    lg={3}>
+                                        <Card
+                                        style={{backgroundColor:this.props.theme.palette.secondary.main, position:'relative', width:'100%', height:'100%'}}>
+                                            <CardContent>
+                                                <Typography variant='h8' style={{color:'white', width:"100vh"}}>ACTIVE CUSTOMERS</Typography>
+                                                <Typography variant='h3' style={{color:'white', width:"100vh"}}>{(this.state.customerCount !== null) ? this.state.activeCustomerCount : 'N/A'}</Typography>
+                                            </CardContent>
+                                            {(this.state.customerCount === null) && (
+                                                <div 
+                                                    style={{
+                                                        position:'absolute',
+                                                        top:0,
+                                                        zIndex:123,
+                                                        width:'100%',
+                                                        height:'100%',
+                                                        backgroundColor:'rgba(230, 230, 230, 0.9)',
+                                                        display:'flex',
+                                                        justifyContent:'center',
+                                                        alignItems:'center'}}>
+                                                    <CircularProgress></CircularProgress>
+                                                </div>)}
+                                    </Card>
+                                </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </div>
