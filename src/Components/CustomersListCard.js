fce159d027595720b791d018b90c8f52628f0119
James Heald <jh97uk@icloud.com>
2020-09-01 19:23:00 -0700

    Added loading functionality to the customer list card

--- src/Components/CustomersListCard.js
+++ src/Components/CustomersListCard.js
@@ -10,11 +10,13 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, TimePicker } from "@material-ui/pickers";
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 class CustomersListCard extends Component{
     constructor(props){
         super(props);
-        this.state = {customers:[]}
+        this.state = {customers:[], loading:true}
         this.timePickerRef = React.createRef()
         this.activateTimePicker = this.activateTimePicker.bind(this);
         this.onTimePicked = this.onTimePicked.bind(this);
@@ -30,6 +32,7 @@ class CustomersListCard extends Component{
 
     onTimePicked(date){
         const self = this;
+        self.setState({loading:true});
         fetch('http://localhost:4000/customer/'+this.currentEditngIndex, {
             method:"PATCH",
             headers:{
@@ -55,7 +58,7 @@ class CustomersListCard extends Component{
                             const updatedCustomerArray = self.state.customers;
                             const id = self.state.customers.indexOf(customer)
                             updatedCustomerArray[id] = {...updatedCustomerArray[id], ...data[0]}
-                            self.setState({customers:updatedCustomerArray})
+                            self.setState({customers:updatedCustomerArray, loading:false})
                             self.props.customerCountSetter(updatedCustomerArray.length, self.getActiveCustomerCount());
                         }
                     });
@@ -85,7 +88,7 @@ class CustomersListCard extends Component{
             }).then(function(response){
                 return response.json();
             }).then(function(data){
-                self.setState({customers:data});
+                self.setState({customers:data, loading:false});
                 self.props.customerCountSetter(data.length, self.getActiveCustomerCount());
 
             })
@@ -107,7 +110,7 @@ class CustomersListCard extends Component{
                     inputRef={this.timePickerRef}
                     onChange={this.onTimePicked}
                     style={{display:'none'}}/>
-            <Card>
+            <Card style={{position:'relative'}}>
                 <CardContent>
                     <TextField
                     id="outlined-required"
@@ -128,9 +131,24 @@ class CustomersListCard extends Component{
                                         + " - " + 
                                         (item.departureTimestamp ? (this.getTimeString(item.departureTimestamp)) : "N/A")}/>
                             </ListItem>
-                        ))}  
+                        ))} 
                     </List>
                 </CardContent>
+                {this.state.loading && 
+                    <div 
+                        style={{
+                            position:'absolute',
+                            top:0,
+                            zIndex:123,
+                            width:'100%',
+                            height:'100%',
+                            backgroundColor:'rgba(230, 230, 230, 0.9)',
+                            display:'flex',
+                            justifyContent:'center',
+                            alignItems:'center'}}>
+                        <CircularProgress></CircularProgress>
+                    </div>}
+                
             </Card>
             </MuiPickersUtilsProvider>
         )
